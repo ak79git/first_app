801d89df21a43e39f116afad66b717a1770e803c
Andy <dev79.php@gmail.com>
2011-10-21 11:40:49 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubystack230::Application.config.secret_token = '76dcf9bd82ad882a805d19146263899dcaa4387570d3bbf06d4cbbf3c9564a6ed6517505f29c04fa1b7a35fda2b50d75d624edf4cb36eb6e50aa84b6f5622f7c'
